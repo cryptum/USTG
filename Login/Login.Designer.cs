9d33ad9fcbff2d24da3216e35525a83ad888b778
danilo <danilovbvip@hotmail.com>
2018-08-21 19:23:30 -0300

    4º Commit

--- Login/Login.Designer.cs
+++ Login/Login.Designer.cs
@@ -40,12 +40,14 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(240, 104);
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(240, 102);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(75, 27);
             this.button1.TabIndex = 0;
             this.button1.Text = "Entrar";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // textBox1
             // 
@@ -53,7 +55,7 @@
             this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.textBox1.Location = new System.Drawing.Point(215, 6);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(133, 29);
+            this.textBox1.Size = new System.Drawing.Size(192, 29);
             this.textBox1.TabIndex = 1;
             // 
             // textBox2
@@ -62,7 +64,7 @@
             this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.textBox2.Location = new System.Drawing.Point(215, 51);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(133, 29);
+            this.textBox2.Size = new System.Drawing.Size(192, 29);
             this.textBox2.TabIndex = 2;
             // 
             // label1
@@ -100,7 +102,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.ClientSize = new System.Drawing.Size(380, 141);
+            this.ClientSize = new System.Drawing.Size(419, 141);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
--- Login/Login.cs
+++ Login/Login.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Configuration;
 using System.Data;
 using System.Drawing;
 using System.Linq;
@@ -15,6 +17,26 @@ namespace Login
         public Login()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string conn = ConfigurationManager.ConnectionStrings["MySQLConnectionString"].ToString();
+            MySqlConnection conexao = new MySqlConnection(conn);
+
+            try
+            {
+                conexao.Open();
+                MessageBox.Show("Deu bom");
+            }
+            catch(MySqlException msqle)
+            {
+                MessageBox.Show("Deu Erro: " + msqle.Message, "ERRO");
+            }
+            finally
+            {
+                conexao.Close();
+            }
         }
     }
 }
